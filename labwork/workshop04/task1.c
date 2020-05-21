1fedb0c9e41f612d804671beb848510385214d0f
Joel Pillar-Rogers <pill0032@flinders.edu.au>
2020-05-21 13:56:22 +0930

    working on workshop4

---
+++ labwork/workshop04/task1.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void usage(char* str){ 
+    fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n", str);
+}
+
+int main(int argc, char *argv[])
+{
+    int flags, opt;
+    int nsecs, tfnd;
+    nsecs = 0;
+    tfnd = 0;
+    flags = 0;
+    while ((opt = getopt(argc, argv, "nt:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'n':
+            flags = 1;
+            break;
+        case 't':
+            nsecs = atoi(optarg);
+            tfnd = 1;
+            break;
+        // ? is returned by getopt if option character is not in opstring, (or option had no argument that required one??) 
+        case '?':   // seems a but redunant, will get picked up by default. better to use : at start of opstring to separate behaviours
+            usage("./task1"); 
+            exit(EXIT_FAILURE);
+        default: 
+            usage("./task1"); 
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n",flags, tfnd, nsecs, optind);
+
+    if(optind >= argc)
+    {
+        fprintf(stderr, "Expected argument after options\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("name argument = %s\n", argv[optind]);
+
+    exit(EXIT_SUCCESS);
+}
