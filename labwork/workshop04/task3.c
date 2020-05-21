e93ee30fd66282e61ff429967975b5ac03a2f429
Joel Pillar-Rogers <pill0032@flinders.edu.au>
2020-05-21 22:17:22 +0930

    finished task 3

--- labwork/workshop04/task3.c
+++ labwork/workshop04/task3.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/time.h>
 
 #define BUFFER_SIZE 1000
 
@@ -59,25 +60,58 @@ int main(int argc, char *argv[]){
 
     if (fflags){
         printf("filename argument = %s\n", filename);
-        if((fd = open(filename, O_RDONLY)) == -1){
-            fprintf(stderr, "Enter a valid filename\n");
-            exit(EXIT_FAILURE);
-        }
-        
-        buffer = (char *)malloc(BUFFER_SIZE); 
-        if (buffer == NULL){
-            exit(1);
-        }
-
-        while((n = read(fd, buffer, BUFFER_SIZE)) > 0){                         
-            write(STDIN_FILENO, buffer, n); 
-        }
         printf("\n");
 
-        free(buffer);
-        buffer = NULL;
+        /* declare variables and get start time of operation */
+        long totTime = 0;
+        long avgTime = 0;
+        int loops = 100000;
+        struct timeval t1;
+        struct timeval t2;
+        gettimeofday(&t1, NULL);
 
-        close(fd);
+        /* main loop */
+        for (int i = 0; i<loops; ++i){
+
+            /* allocate memory */
+            buffer = (char *)malloc(BUFFER_SIZE); 
+            if (buffer == NULL){
+                exit(1);
+            }
+
+            /*open file*/
+            if((fd = open(filename, O_RDONLY)) == -1){
+                fprintf(stderr, "Enter a valid filename\n");
+                exit(EXIT_FAILURE);
+            } 
+
+            /* read content of file into buffer*/
+            while((n = read(fd, buffer, BUFFER_SIZE)) > 0){   
+                /* print buffer*/                      
+                write(STDIN_FILENO, buffer, n); 
+            }
+            printf("\n");
+            printf("\n");
+
+            /* close file*/
+            close(fd);
+
+            /* free memory */
+            free(buffer);
+            buffer = NULL;
+        }
+        /* get finish time of operation */
+        gettimeofday(&t2, NULL);
+
+        /* calculate total andd average times*/
+        totTime = 1000000 * (t2.tv_sec - t1.tv_sec);
+        totTime += (t2.tv_usec - t1.tv_usec); 
+        avgTime = totTime / loops;
+
+        /* display elapsed time for operations */
+        printf("loops: %d\n",loops);
+        printf("elapsed time (usecs): %ld\n",totTime);
+        printf("average time per loop (usecs): %ld\n",avgTime);
     }
     else {
         printf("No file submitted\n");
